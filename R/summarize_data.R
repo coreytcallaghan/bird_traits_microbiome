f09ac0e71626283f78df6d69b8cc1b265802cc8f
Corey Callaghan <callaghan.corey.t@gmail.com>
2023-08-03 09:05:53 -0400

    updated data and code for submission

--- R/summarize_data.R
+++ R/summarize_data.R
@@ -8,7 +8,7 @@ library(forcats)
 library(scales)
 
 # prepare trait data
-analysis_dat <- readRDS("Clean data/analysis_data.RDS") %>%
+analysis_dat <- readRDS("Clean data/analysis_data_alpha.RDS") %>%
   dplyr::filter(complete.cases(ebird_COMMON_NAME))
 
 length(unique(analysis_dat$ebird_COMMON_NAME))
